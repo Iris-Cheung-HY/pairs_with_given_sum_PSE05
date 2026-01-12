c9e939f81513d956c3e3a2648661d2000c7d1659
Iris Cheung <hokyinirischeung@gmail.com>
2026-01-11 19:40:11 -0800

    solved again

--- main/main.py
+++ main/main.py
@@ -1,14 +1,32 @@
 
 def pairs_with_given_sum(numbers, target):
-    pair_list = []
-    pair_count = 0
+    numbers.sort()
+    n = len(numbers)
+    l = 0
+    r = n - 1
+    pair = 0
 
-    for num in numbers[:]:
-        diff = target - num
-        if diff in pair_list:
-            pair_count += 1
-            pair_list.remove(diff)
-            numbers.remove(num)
-        else:
-            pair_list.append(num)
-    return pair_count
+    while l < r:
+        s = numbers[l] + numbers[r]
+        if s == target:
+            pair += 1
+            l += 1
+        if s > target:
+            r -= 1
+        if s < target:
+            l += 1
+    return pair
+
+# Solution 2
+#     pair_list = []
+#     pair_count = 0
+
+#     for num in numbers[:]:
+#         diff = target - num
+#         if diff in pair_list:
+#             pair_count += 1
+#             pair_list.remove(diff)
+#             numbers.remove(num)
+#         else:
+#             pair_list.append(num)
+#     return pair_count
